b34419795fcc32e9fcd38129c5c9a5d4314cc3bb
Thomas Burk <t.burk@shellraiser.com>
2020-07-14 13:37:31 +0200

    DB connection details have been commented out, in order to use this app without DB for demo purposes.

--- PowerSquare/DataAccess/MainSqlConnector.cs
+++ PowerSquare/DataAccess/MainSqlConnector.cs
@@ -4,6 +4,7 @@ using PowerSquare.Models;
 using PowerSquare.ViewModels;
 using System.Data.SqlClient;
 
+// In order to use this app without DB access, I've commented out all affected parts.
 
 namespace PowerSquare.DataAccess
 {
@@ -17,38 +18,40 @@ namespace PowerSquare.DataAccess
 
         public bool GetAllCalculations()
         {
-            SqlDataReader sqlDataReader = SqlAction.GetQuery(
-                "SELECT Number, Operation FROM PowerSquare;"
-                );
+            //SqlDataReader sqlDataReader = SqlAction.GetQuery(
+            //    "SELECT Number, Operation FROM PowerSquare;"
+            //    );
 
-            if (sqlDataReader.HasRows)
-            {
-                viewModel.Calculations.Clear();
-                while (sqlDataReader.Read())
-                {
-                    viewModel.Calculations.Add(new CalculationModel(
-                        sqlDataReader.SafeGetInt32(0),
-                        sqlDataReader.SafeGetString(1)
-                        ));
-                }
-            }
+            //if (sqlDataReader.HasRows)
+            //{
+            //    viewModel.Calculations.Clear();
+            //    while (sqlDataReader.Read())
+            //    {
+            //        viewModel.Calculations.Add(new CalculationModel(
+            //            sqlDataReader.SafeGetInt32(0),
+            //            sqlDataReader.SafeGetString(1)
+            //            ));
+            //    }
+            //}
             return true;
         }
 
         public bool ClearAllCalculations()
         {
-            return SqlAction.ExecuteNonQuery(
-                "DELETE FROM PowerSquare;"
-                );
+            //return SqlAction.ExecuteNonQuery(
+            //    "DELETE FROM PowerSquare;"
+            //    );
+            return true;
         }
 
         public bool StoreCalculation()
         {
-            return SqlAction.ExecuteNonQuery(
-                "INSERT INTO PowerSquare VALUES(@Parameter0, @Parameter1);",
-                viewModel.Number,
-                viewModel.OperationMode
-                );
+            //return SqlAction.ExecuteNonQuery(
+            //    "INSERT INTO PowerSquare VALUES(@Parameter0, @Parameter1);",
+            //    viewModel.Number,
+            //    viewModel.OperationMode
+            //    );
+            return true;
         }
     }
 }
